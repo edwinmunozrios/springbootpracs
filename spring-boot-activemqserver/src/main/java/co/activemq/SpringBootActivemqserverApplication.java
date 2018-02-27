e40aa6726df2c76acf0261e314a53771090a4dcb
edwimuri <edwimuri@PEPS0020024740.suramericana.com.co>
2018-02-27 12:46:53 -0500

    First consumer

---
+++ spring-boot-activemqserver/src/main/java/co/activemq/SpringBootActivemqserverApplication.java
@@ -0,0 +1,14 @@
+package co.activemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
+
+@SpringBootApplication
+@EnableJms
+public class SpringBootActivemqserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActivemqserverApplication.class, args);
+	}
+}
